32968617de5d9f3464d982be368f290594651a71
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-01-18 19:59:07 -0600

    feat: Added an api get point for the portfolio performance function.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Express } from "express";
 // Initialize Express application
 const app: Express = express();
 
+import { calculatePortfolioPerformance } from "./portfolio/portfolioPerformance";
 /**
  * Represents the response structure for a health check endpoint
  */
@@ -25,4 +26,16 @@ app.get("/api/v1/health", (req, res) => {
 
     res.json(healthData);
 });
+
+app.get("/api/v1/portfolio/performance", (req, res) => {
+    const initialInvestment = Number(req.query.initialInvestment);
+    const currentValue = Number(req.query.currentValue);
+    if (!initialInvestment || !currentValue) {
+    return res.status(400).json({ error: "initialInvestment and currentValue are required" });
+    }
+
+    const calculatedPerformance = calculatePortfolioPerformance(currentValue, initialInvestment);
+    res.json(calculatedPerformance);
+});
+
 export default app;
